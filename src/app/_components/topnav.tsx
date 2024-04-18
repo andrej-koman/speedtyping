da34783980b4346f435db223af3cdc32b8c8efdf
andrej-koman <andrej.koman123@gmail.com>
2024-04-18 19:31:13 +0200

    fix: Beta clerk with page side auth instead of project side

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -5,14 +5,12 @@ export default function TopNav() {
     return (
         <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b">
             <Logo />
-            <div>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-            </div>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
         </nav>
     )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({});
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) auth().protect();
+});
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
