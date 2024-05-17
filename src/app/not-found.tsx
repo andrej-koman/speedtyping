67f8b3052c07c2f4ed75f3d7fe4043fbd8ef1f61
andrej-koman <andrej.koman123@gmail.com>
2024-05-17 17:18:50 +0200

    feat: Traffic animation on not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import "./not-found.css";
 import { Separator } from "~/components/ui/separator";
 import Logo from "./_components/logo";
+import TrafficLight from "~/illustrations/traffic-light-illustration";
 
 function NotFoundLink({
   href,
@@ -11,7 +12,10 @@ function NotFoundLink({
   children: React.ReactNode;
 }) {
   return (
-    <Link className="hover:text-primary hover:underline underline-offset-4" href={href}>
+    <Link
+      className="underline-offset-4 hover:text-primary hover:underline"
+      href={href}
+    >
       {children}
     </Link>
   );
@@ -19,7 +23,7 @@ function NotFoundLink({
 
 export default function NotFound() {
   return (
-    <div className="not-found grid h-screen w-screen grid-cols-3 grid-rows-4">
+    <div className="not-found grid h-screen w-screen grid-cols-3 grid-rows-4 overflow-hidden">
       <div className="col-start-1 row-span-2 row-start-2 ms-auto flex flex-col justify-start">
         <Logo href="/" size="lg" />
         <h1 className="text-[150px]">404</h1>
@@ -29,6 +33,9 @@ export default function NotFound() {
           <Separator orientation="vertical" />
           <NotFoundLink href="/search">Search</NotFoundLink>
         </div>
+      </div>
+      <div className="relative col-span-2 col-start-2 row-span-4 flex items-center justify-center">
+        <TrafficLight />
       </div>
     </div>
   );
